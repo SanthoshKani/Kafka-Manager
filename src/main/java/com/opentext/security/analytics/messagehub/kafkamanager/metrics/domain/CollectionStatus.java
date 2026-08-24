f6d17266bee164b659f8136f9905f3c422e07a15
Santhosh Kani Surulirajan <skani@opentext.com>
2026-08-24 14:12:17 +0530

    Permanently remove metrics sources and tests

--- src/main/java/com/opentext/security/analytics/messagehub/kafkamanager/metrics/domain/CollectionStatus.java
+++
@@ -1,7 +0,0 @@
-package com.opentext.security.analytics.messagehub.kafkamanager.metrics.domain;
-
-public enum CollectionStatus {
-    SUCCESS,
-    FAILURE,
-    UNKNOWN
-}
--- src/main/java/com/opentext/security/analytics/messagehub/kafkamanager/metrics/runtime/MetricSemanticType.java
+++
@@ -1,9 +0,0 @@
-package com.opentext.security.analytics.messagehub.kafkamanager.metrics.runtime;
-
-/**
- * Semantic type of a metric sample.
- */
-public enum MetricSemanticType {
-    MONOTONIC_COUNTER,
-    GAUGE
-}
